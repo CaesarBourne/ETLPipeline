6d33b23b957c2d7f6d489a3f02f7de5b3d3146b4
Emmanuel Adeleke <caesaradek@gmail.com>
2026-03-28 12:04:46 +0000

    Remove unused ES module imports and streamline producer.js

--- src/etl/producer.js
+++ src/etl/producer.js
@@ -1,11 +1,4 @@
-// import fs from "fs";
-// import path from "path"
-// import csv from "csv-parser";
-// import queue from "./queue.js";
-// import { fileURLToPath } from "url"
 
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
 const fs = require("fs")
 const path = require("path")
 const csv = require("csv-parser")
